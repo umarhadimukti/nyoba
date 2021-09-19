d2bbbc1770289911e13b7c8cb4b9ca284e27cefe
umarhadimukti <76905897+umarhadimukti@users.noreply.github.com>
2021-09-19 10:00:28 +0700

    menambahkan rumus luas persegi

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let alas = 7;
 let tinggi = 13;
 const luasSegitiga = (1 / 2) * alas * tinggi;
 console.log(luasSegitiga);
+
+let sisi = 4;
+const luasPersegi = sisi * sisi;
+console.log(luasPersegi);
